1f2769f7d238470fcc595d9de0bef0eefb27becd
도미닉 <kiljh@me.com>
2020-05-22 22:09:21 +0900

    프로젝트 생성

---
+++ maumNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  maumNote
+//
+//  Created by joon-ho kil on 2020/05/22.
+//  Copyright © 2020 joon-ho kil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
