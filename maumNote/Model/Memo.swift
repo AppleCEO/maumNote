3e174f67f502867a920dbe62d8abb36d25464f6f
도미닉 <kiljh@me.com>
2020-05-23 11:33:11 +0900

    Memo 가 저장될 모델 생성

---
+++ maumNote/Model/Memo.swift
@@ -0,0 +1,14 @@
+//
+//  Memo.swift
+//  maumNote
+//
+//  Created by joon-ho kil on 2020/05/23.
+//  Copyright © 2020 joon-ho kil. All rights reserved.
+//
+
+import Foundation
+
+struct Memo {
+  let title: String
+  let content: String
+}
