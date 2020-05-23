1af9ad3cd78511df142f2fb69c675f026da6a4c8
도미닉 <kiljh@me.com>
2020-05-23 23:09:20 +0900

    테스트 파일 생성

---
+++ maumNoteTests/maumNoteTests.swift
@@ -0,0 +1,25 @@
+//
+//  maumNoteTests.swift
+//  maumNoteTests
+//
+//  Created by joon-ho kil on 2020/05/23.
+//  Copyright © 2020 joon-ho kil. All rights reserved.
+//
+
+import XCTest
+
+class maumNoteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
